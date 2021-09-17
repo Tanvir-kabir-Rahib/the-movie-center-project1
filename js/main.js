bd08b39e15b164bc3f346dc7591e3324989ea484
Tanvir-kabir-Rahib <70778768+Tanvir-kabir-Rahib@users.noreply.github.com>
2021-09-17 11:02:40 +0600

    api key comment added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
 console.log("Welcome to the movie center console.")
+// <!-- 2b1c1fed5f817943ce9cd2954b2505d1 -->
+// <!-- https://api.themoviedb.org/3/movie/550?api_key=2b1c1fed5f817943ce9cd2954b2505d1 -->
